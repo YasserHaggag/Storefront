44547bd0c13aba667e57a90ea3eb98eea1253775
Yasser Hassan <E-y.ali@lean.sa>
2022-05-19 11:05:03 +0300

    Remove comments from Server and users.routes files

--- dist/routes/user.routes.js
+++ dist/routes/user.routes.js
@@ -9,39 +9,5 @@ userRoutes.post('/', user_controllers_1.create);
 userRoutes.delete('/:id', user_controllers_1.deleteUser);
 userRoutes.put('/', user_controllers_1.updateUser);
 userRoutes.post('/authenticate', user_controllers_1.authenticate);
-/* userRoutes.post('/', (req: Request, res: Response) => {
-    const article: users = {
-      name: req.body.name,
-      content: req.body.content
-    }
-    try {
-       res.send('this is the CREATE route')
-    } catch (err) {
-       res.status(400)
-       res.json(err)
-    }
-}) */
-/* userRoutes.put('/users/:id', (req: Request, res: Response) => {
- const article: Article = {
-      id: req.params.id,
-      title: req.body.title,
-      content: req.body.content
-    }
-    try {
-       res.send('this is the EDIT route')
-    } catch (err) {
-       res.status(400)
-       res.json(err)
-    }
-}) */
-/* userRoutes.delete('/articles/:id', (_req: Request, res: Response) => {
-    try {
-       res.send('this is the DELETE route')
-    } catch (err) {
-       res.status(400)
-       res.json(err)
-    }
-}
 
-) */
 exports.default = userRoutes;
--- src/server.ts
+++ src/server.ts
@@ -10,29 +10,16 @@ const user=new users()
 const app= express();
 const port=3000;
 
-//app.use(validateTokenMiddleware)
-
-
 app.use(express.json(),validateTokenMiddleware);
 
 app.use('/api',routes)
 
-/* app.get('/api',  async function(req:Request,res:Response)
-{//res.send('hello');
-    const resu=  await user.getall();
-    
-    res.send(resu)
-    
-}); */
 
 app.listen(port,()=>
 {
     console.log(`Server started at localhost: ${port}`)
 })
 app.use(morgan('common'))
-
-
-//app.use(validateTokenMiddleware)
 
 app.use(errormid)
 export default app
